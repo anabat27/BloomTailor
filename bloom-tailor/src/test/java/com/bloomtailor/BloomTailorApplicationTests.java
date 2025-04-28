339a67fe578023a43ebe7277a05a5a3cd29b458a
Denis <163418752+adenis033@users.noreply.github.com>
2025-04-28 13:58:01 +0300

    Added Landpage BloomTailor
    Added the first page that will pop when you open the website.

---
+++ bloom-tailor/src/test/java/com/bloomtailor/BloomTailorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloomtailor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomTailorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
